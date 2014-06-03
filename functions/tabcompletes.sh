64f10f41ad9537241aadcc25b21edd7420ac13f9
robink <robink@zoosk.com>
2014-06-03 04:12:51 +0000

    Add tab completion for workspace names and workers
    git-svn-id: https://s.zoosk.com/zoosk-tools/trunk/zenv@148837 046000e8-c612-472f-bb51-fe359d1450d5

---
+++ functions/tabcompletes.sh
@@ -0,0 +1,28 @@
+##
+# This file contains the autocomplete setup functions for various programs.
+#
+
+
+## use/zenv_use
+_tabcomp_use() {
+    local cur workspaces
+    workspaces=$(/bin/ls -F ${ZENV_WORKSPACE}/ | egrep "/$" | cut -d / -f 1)
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=( $(compgen -W "${workspaces}" -- ${cur}) )
+}
+complete -F _tabcomp_use zenv_use
+complete -F _tabcomp_use use
+
+
+## worker
+_tabcomp_worker() {
+    local cur workers
+    if [ "$ZENV_SERVERDIR" != '' ]; then
+        workers=$(/bin/ls ${ZENV_SERVERDIR}/current/web/services/jobworkers | cut -d / -f 1)
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        COMPREPLY=( $(compgen -W "${workers}" -- ${cur}) )
+    else
+        COMPREPLY=( $(compgen '' -- ${cur}) )
+    fi
+}
+complete -F _tabcomp_worker worker
