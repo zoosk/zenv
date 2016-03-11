582dbba5687443ce2a06381c164b29e3cc3f01b9
Robin Keller <robink@zoosk.com>
2016-03-11 10:20:42 -0800

    More helpful docs in default.setup.sh

--- setupscripts/default.setup.sh
+++ setupscripts/default.setup.sh
@@ -7,3 +7,7 @@
 # For setup behavior specific to a given checkout type, create a script in this
 # directory named <checkout_type>.setup.sh. For example, if my repo was named
 # 'zoosk', I would create a file called 'zoosk.setup.sh'.
+
+# If you need to write code that runs each time a checkout is used as opposed to
+# when it is first set up, add your code to properties/default.work.properties
+# instead.
