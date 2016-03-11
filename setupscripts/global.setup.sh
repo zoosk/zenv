07bcee428f7aa9ef95b10e8bba64088e094282d1
Robin Keller <robink@zoosk.com>
2016-03-10 17:12:16 -0800

    Add note about global.setup.sh vs the environment folder to global.setup.sh

--- setupscripts/global.setup.sh
+++ setupscripts/global.setup.sh
@@ -2,3 +2,7 @@
 
 # This script will run just before the ZEnv install process completes. If it
 # exits with a 0 exit code, the installation is considered a success.
+
+# It's best if you only put things that _need_ to be first-time here.
+# Settings that are used on a daily basis should go in the environment/
+# folder instead.
